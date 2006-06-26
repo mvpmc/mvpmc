f31efcf82efc33a649e109909c0ffb58bd873438
Jon Gettler <gettler@acm.org>
2006-06-25 21:37:59 -0500

    Rearrange the building of mysql and ncurses to use scons.

--- scons/build_lib.sh
+++ scons/build_lib.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 #
-# $Id: build_lib.sh 20 2006-03-14 05:26:56Z gettler $
-#
 # build_app.sh - build a library
 #
 
@@ -12,23 +10,29 @@ help() {
     echo "	-h         print this help"
     echo "	-i file    input file to build"
     echo "	-I dir     install directory"
+    echo "	-L opts    LDFLAGS options"
     echo "	-p file    patch file (applied in order)"
     exit 0
 }
 
+MYPWD=`pwd`
 PATCHES=
 INPUT=
 DIR=
+AUTOCONF=0
 
-while getopts "c:C:d:hi:I:p:" i
+while getopts "Ac:C:d:hi:I:L:p:T:" i
   do case "$i" in
+      A) AUTOCONF=1;;
       c) CROSS=$OPTARG;;
       C) CONFIG_OPTS=$OPTARG;;
       d) DIR=$OPTARG;;
       h) help ;;
       i) INPUT=$OPTARG;;
       I) INSTALL=$OPTARG;;
+      L) LDFLAGS=$OPTARG;;
       p) PATCHES="$PATCHES $OPTARG";;
+      T) TARGET=$OPTARG;;
       *) echo error ; exit 1 ;;
   esac
 done
@@ -39,6 +43,7 @@ echo "DIR: $DIR"
 echo "CROSS: $CROSS"
 echo "INSTALL: $INSTALL"
 echo "CONFIG_OPTS: $CONFIG_OPTS"
+echo "AUTOCONF: $AUTOCONF"
 
 if [ "$DIR" = "" ] ; then
     echo "Application directory not specified!"
@@ -71,6 +76,7 @@ esac
 cd $DIR || exit 1
 
 for i in $PATCHES ; do
+    echo "Applying patch $i..."
     patch -p1 < ../../$i || exit 1
 done
 
@@ -79,6 +85,32 @@ export CROSS_PREFIX=$CROSS
 export CC=${CROSS}gcc
 export INSTALL_PREFIX=$INSTALL
 export TOPLEVEL=$INSTALL
+export LDFLAGS
+
+if [ "`basename $PWD`" = "mysql-5.0.21" ] ; then
+    set -x
+    export CXX=${CROSS}g++
+    export CPP=${CROSS}cpp
+    unset LDFLAGS
+    unset CROSS
+    unset CFLAGS
+    unset CXXFLAGS
+    if [ "$TARGET" = "mvp" ] ; then
+	patch -p0 < $MYPWD/scons/configure.patch
+    fi
+    export C_INCLUDE_PATH=$MYPWD/dongle/install/$TARGET/include
+    export CFLAGS=-I$MYPWD/dongle/install/$TARGET/include
+    export LDFLAGS=-L$MYPWD/dongle/install/$TARGET/lib
+    ./configure $CONFIG_OPTS --prefix=$INSTALL
+    make
+    make install
+    exit $?
+fi
+
+if [ $AUTOCONF -eq 1 ] ; then
+    echo "Running autoconf..."
+    autoconf
+fi
 
 if [ -f configure ] ; then
     ./configure --prefix=$INSTALL $CONFIG_OPTS
