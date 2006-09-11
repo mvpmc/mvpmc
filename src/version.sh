da423f920f28500915d71939ed86a50ba8155f56
Simon Hyde <simon@icedrop.net>
2006-09-11 09:58:03 +0100

    Indicate whether local changes have been made in the git revision number

--- src/version.sh
+++ src/version.sh
@@ -2,14 +2,17 @@
 
 cd $1
 
-echo char compile_time[] = \"`LANG=C date`\" \; > version.c
-echo char version_number[] =  \"$VERSION\"\; >> version.c
-echo char build_user[] =  \"$USER\"\; >> version.c
+echo char compile_time[] = \"`LANG=C date`\" \; > "$2/version.c"
+echo char version_number[] =  \"$VERSION\"\; >> "$2/version.c"
+echo char build_user[] =  \"$USER\"\; >> "$2/version.c"
 
 GIT_REV_LIST=`which git-rev-list`
 if [ "$GIT_REV_LIST" != "" ] ; then
     GIT_REVISION=`git-rev-list --all | head -1`
+    if [ `git diff $GIT_REVISION | grep "^[+-]" | head -1 | wc -l` -ne 0 ]; then
+	GIT_REVISION="$GIT_REVISION with local changes"
+    fi
 else
     GIT_REVISION=
 fi
-echo char git_revision[] =  \"$GIT_REVISION\"\; >> version.c
+echo char git_revision[] =  \"$GIT_REVISION\"\; >> "$2/version.c"
